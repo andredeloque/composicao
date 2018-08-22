a377de546ed7c0c875c414496087362d2c960022
André Lopes <38387474+andredeloque@users.noreply.github.com>
2018-08-22 17:42:38 -0300

    Add files via upload

---
+++ src/main/java/br/com/db1/oo/PessoaFisica.java
@@ -0,0 +1,17 @@
+package br.com.db1.oo;
+
+public class PessoaFisica extends Pessoa {
+	private String cpf;
+
+public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+public Boolean validadorCpf(){
+	return Boolean.TRUE;
+}
+}
---
+++ src/main/java/br/com/db1/oo/PessoaJuridica.java
@@ -0,0 +1,5 @@
+package br.com.db1.oo;
+
+public class PessoaJuridica {
+
+}
